57dcfe68813724f1be5199e91caf995e146428e8
Prakhar Porwal <89337486+prakhar0x01@users.noreply.github.com>
2023-09-16 18:09:38 +0530

    Update index.js
    Adding a prank and meme to main page

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,11 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h2>Gentle Reminder : This Page is only for Testing Purpose...</h2>
 
       <main>
+        <br><br><hr><h2>If you're here you probably come from my github profile...</h2>
+        <img src=https://i.imgflip.com/p67ab.jpg onmouseover=confirm("This is not XSS... 😂😂😂")>
         <Header title="Welcome to my app!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
