6646f621983a99f92c2deed1860e788a32ec6d3b
Prakhar Porwal <89337486+prakhar0x01@users.noreply.github.com>
2023-09-16 18:29:21 +0530

    Update index.js
    Mistake Again happened....well ignore that and instead of focusing in my commits focus on your studies....

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,12 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h2>Gentle Reminder : This Page is only for Testing Purpose...</h2>
 
       <main>
-        <br><br><hr><h2>If you're here you probably come from my github profile...</h2>
-        <img src="https://i.imgflip.com/p67ab.jpg" onmouseover=confirm("This is not XSS... 😂😂😂")>
         <Header title="Welcome to my app!" />
+        <h2>Gentle Reminder : This Page is only for Testing Purpose...</h2>
+        <br><br><h2>If you're here you probably come from my github profile...</h2>
+        <img src="https://i.imgflip.com/p67ab.jpg" onmouseover=confirm("This is not XSS... 😂😂😂")>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
